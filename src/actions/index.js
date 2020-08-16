4629901982ff8b094c5173d603f6aef1474bcab8
Anmol Singh <anmolsingh123.as@gmail.com>
2020-08-16 09:40:35 +0530

    changing the state to add favourite as well

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,18 @@
-{
-    type: 'ADD_MOVIES'
+// {
+//     type: 'ADD_MOVIES'
+// }
+// {
+//     type: 'DECREASE_COUNT'
+// }    
+
+
+// action types 
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+// action creaters
+export function ADD_MOVIES(movies){
+    return {
+        type : ADD_MOVIES,
+        movies 
+    }
 }
-{
-    type: 'DECREASE_COUNT'
-}    
--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,43 @@
 import React from "react";
-import { data } from "../data";
+//import { data } from "../data";
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
+import { addMovies } from "../actions";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-          <div className="tab">Movies</div>
-          <div className="tab">Favourites</div>
-        </div>
+class App extends React.Component {
+  componentDidMount(){
+    const {store} = this.props; 
+    store.subscribe(()=>{
+      console.log('updated');
+      this.forceUpdate();
+      
+    });
+    //make api call
+    //dispatch action
+    store.dispatch(ADD_MOVIES(data));
 
-        <div className="list">
-          {data.map((movie) => (
-            <MovieCard movie={movie} />
-          ))}
+    console.log('STATE',this.props.store.getState());
+    
+  }
+  render() {
+    const {list} = this.props.store.getState();
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favourites</div>
+          </div>
+
+          <div className="list">
+            {list.map((movie, index) => (
+              <MovieCard movie={movie} key={`movies-${index}`} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default App;
